a58f77d3d7c357df2a3617c935f11ae6b29160a4
riazahmedshah <riyazsh360@gmail.com>
2025-02-16 22:52:24 +0530

    login/logout

--- src/types/signinTypes.ts
+++ src/types/signinTypes.ts
@@ -1,6 +1,6 @@
 import {z} from "zod"
 
-export const signinSchemaType = z.object({
+export const signinType = z.object({
     email: z.string().email("provide valid email").trim().toLowerCase(),
     password: z.string().min(6,"Password must have six characters"),
 })

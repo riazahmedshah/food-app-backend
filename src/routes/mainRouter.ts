0b26a8822e92a8de5a343905b7763d8ada751347
riazahmedshah <riyazsh360@gmail.com>
2025-02-18 17:30:46 +0530

    added rolecheck,admin route etc..

--- src/routes/mainRouter.ts
+++ src/routes/mainRouter.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { authRouter } from "./authRouter";
 import { resRouter } from "./restaurantsRouter";
+import { adminRouter } from "./adminRouter";
 
 export const mainRouter = express.Router();
 
 mainRouter.use("/auth",authRouter);
 mainRouter.use("/restaurant", resRouter);
+mainRouter.use("/admin",adminRouter);

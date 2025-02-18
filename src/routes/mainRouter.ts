1588d2a10346ec0cc5a876eaf652ddc95003a141
riazahmedshah <riyazsh360@gmail.com>
2025-02-18 17:55:42 +0530

    profile | profile/edit route

--- src/routes/mainRouter.ts
+++ src/routes/mainRouter.ts
@@ -2,9 +2,11 @@ import express from "express";
 import { authRouter } from "./authRouter";
 import { resRouter } from "./restaurantsRouter";
 import { adminRouter } from "./adminRouter";
+import { profileRouter } from "./profileRouter";
 
 export const mainRouter = express.Router();
 
 mainRouter.use("/auth",authRouter);
 mainRouter.use("/restaurant", resRouter);
 mainRouter.use("/admin",adminRouter);
+mainRouter.use("/profile",profileRouter);

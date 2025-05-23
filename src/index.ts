0fa50d6aecbdcd4242a970d298cde5c8a2fbad94
riazahmedshah <riyazsh360@gmail.com>
2025-05-23 15:56:44 +0530

    added cors lib

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,17 @@ import express from "express"
 import { ConnectDB } from "./config/db";
 import { mainRouter } from "./routes/mainRouter";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 const PORT = 1200;
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173", 
+  credentials: true,
+  methods: ["GET", "POST", "PATCH", "DELETE"], 
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 app.use(express.json());
 app.use(cookieParser());
